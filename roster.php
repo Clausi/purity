ab3698afbf1277c0651686c2ec9b211ea3f72712
Clausi <github@cklausi.de>
2014-10-07 14:10:58 +0200

    add exmember section for admin

--- roster.php
+++ roster.php
@@ -5,22 +5,39 @@ if (!defined('IN_PHPBB'))
 	exit;
 }
 
-$query = "SELECT * FROM ".$TableNames['roster']." WHERE active = '1' ORDER BY name";
+$query = "SELECT * FROM ".$TableNames['roster']." ORDER BY name";
 $result = $db->sql_query($query);
 
 while ($row = $db->sql_fetchrow($result))
 {
-	$template->assign_block_vars('n_members', array(
-		'NAME' => $row['name'],
-		'RANK' => $row['rank'],
-		'RANKNAME' => getRank($row['rank'], $ranks),
-		'LEVEL'	=> $row['level'],
-		'ILEVEL' => $row['iLevel'],
-		'CLASS'	=> $row['class'],
-		'RACE' => $row['race'],
-		'GENDER' => $row['gender'],
-		'ACHIEVEMENTPOINTS' => $row['achievementPoints'],
-	));
+	if($row['active'] == 0)
+	{
+		$template->assign_block_vars('n_exMembers', array(
+			'NAME' => $row['name'],
+			'RANKNAME' => getRank($row['rank'], $ranks),
+			'LEVEL'	=> $row['level'],
+			'ILEVEL' => $row['iLevel'],
+			'CLASS'	=> $row['class'],
+			'RACE' => $row['race'],
+			'GENDER' => $row['gender'],
+			'LASTSEEN' => $user->format_date(strtotime($row['cache']), false, true)
+		));
+	}
+	else
+	{
+		$template->assign_block_vars('n_members', array(
+			'NAME' => $row['name'],
+			'RANK' => $row['rank'],
+			'RANKNAME' => getRank($row['rank'], $ranks),
+			'LEVEL'	=> $row['level'],
+			'ILEVEL' => $row['iLevel'],
+			'CLASS'	=> $row['class'],
+			'RACE' => $row['race'],
+			'GENDER' => $row['gender'],
+			'ACHIEVEMENTPOINTS' => $row['achievementPoints'],
+			'FIRSTSEEN' => $user->format_date(strtotime($row['firstseen']), false, true)
+		));
+	}
 }
 
 $db->sql_freeresult($result);
