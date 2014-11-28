a74c0e87d8f9ae6405bcf964ba22c4eb612dc11f
Clausi <github@cklausi.de>
2014-11-28 08:12:39 +0100

    merge character update into one cron

--- cron/character_ilvl.php
+++ cron/character_ilvl.php
@@ -1,43 +1,25 @@
 <?php
 
-define('IN_PHPBB', true);
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './../../';
-$phpEx = substr(strrchr(__FILE__, '.'), 1);
-include($phpbb_root_path . 'common.' . $phpEx);
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
 
-include($phpbb_root_path . 'guild/includes/constants.' . $phpEx);
-include($phpbb_root_path . 'guild/includes/functions.' . $phpEx);
-include($phpbb_root_path . 'guild/includes/wowarmoryapi.' . $phpEx);
-
-
-$query = "SELECT uniquekey, name, realm FROM " . $TableNames['roster'] . " WHERE active = '1' ORDER BY lastupdate ASC LIMIT 1";
-$result = $db->sql_query($query);
-$row = $db->sql_fetchrow($result);
-$Character = $row['name'];
-echo $uniquekey = $row['uniquekey'];
-echo "<br />";
-echo $Character;
-echo "<br />";
-
-$realm = $row['realm'];
-if($realm == NULL) $realm = $GuildRealm;
-
-echo $realm;
-echo "<br />";
-echo $GuildRegion;
-
-$armory = new BattlenetArmory($GuildRegion, $realm);
-$armory->setLocale($armoryLocale);
-//$armory->UTF8(TRUE);
-$armory->setCharactersCacheTTL($WGPConfig['Cache']);
-
-$CharacterData = $armory->getCharacter($Character);
 $gear = $CharacterData->getGear();
+
+if( ! is_array($gear)) 
+{
+	$query = "UPDATE " . $TableNames['roster'] . " SET lastupdate = NOW() WHERE uniquekey = '".$uniquekey."'";
+	$result = $db->sql_query($query);
+	trigger_error("No gear array, character not reachable", E_USER_ERROR);
+	exit;
+}
 
 if($gear['averageItemLevelEquipped'] != '')
 {
 	$query = "UPDATE " . $TableNames['roster'] . " SET iLevel = '".$gear['averageItemLevelEquipped']."' WHERE uniquekey = '".$uniquekey."'";
 	$result = $db->sql_query($query);
+	echo "<br />ilvl: ".$gear['averageItemLevelEquipped'];
 }
 
 // echo "<pre>";
--- cron/character_progress.php
+++ cron/character_progress.php
@@ -1,37 +1,10 @@
 <?php
 
-define('IN_PHPBB', true);
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './../../';
-$phpEx = substr(strrchr(__FILE__, '.'), 1);
-include($phpbb_root_path . 'common.' . $phpEx);
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
 
-include($phpbb_root_path . 'guild/includes/constants.' . $phpEx);
-include($phpbb_root_path . 'guild/includes/functions.' . $phpEx);
-include($phpbb_root_path . 'guild/includes/wowarmoryapi.' . $phpEx);
-
-
-$query = "SELECT uniquekey, name, realm FROM " . $TableNames['roster'] . " WHERE active = '1' ORDER BY lastupdate ASC LIMIT 1";
-$result = $db->sql_query($query);
-$row = $db->sql_fetchrow($result);
-$Character = $row['name'];
-echo $uniquekey = $row['uniquekey'];
-echo "<br />";
-echo $Character;
-echo "<br />";
-
-$realm = $row['realm'];
-if($realm == NULL) $realm = $GuildRealm;
-
-echo $realm;
-echo "<br />";
-echo $GuildRegion;
-
-$armory = new BattlenetArmory($GuildRegion, $realm);
-$armory->setLocale($armoryLocale);
-//$armory->UTF8(TRUE);
-$armory->setCharactersCacheTTL($WGPConfig['Cache']);
-
-$CharacterData = $armory->getCharacter($Character);
 $Raidprogress = $CharacterData->getRaidStats('desc');
 
 if( ! is_array($Raidprogress)) 
@@ -337,3 +310,5 @@ while($row = $db->sql_fetchrow($result)) {
 	$query2 = "UPDATE " . $TableNames['progress'] . " SET active = '".$active."' WHERE raidid = '". $row['raidid'] ."'";
 	$result2 = $db->sql_query($query2);
 }
+
+echo "<br />progress";
